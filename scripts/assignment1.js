80f8f2aa1c4a6edab1b1f8aca2491be25b45d6e3
ngreenfield <nagreenfield@yahoo.com>
2024-12-10 20:12:00 -0800

    assignment 1

---
+++ scripts/assignment1.js
@@ -0,0 +1,27 @@
+//using let
+let character = "Dennis";
+let action = "walking";
+let thing = "red door";
+let place = "Santa's shop";
+let time = "midnight";
+
+//using const
+const holdiay = "Christmas";
+
+//creating the story
+console.log("---Story=--")
+console.log(character + " was " + action + " down the street at " + time + " when suddenly they noticed a " + thing + ". " + "As " + character + " went through the " + thing +", " + character + " was transported to " + place + " where all the workers were getting ready for " + holdiay +".");
+
+//changing values
+console.log("--Differnt Story--")
+
+character = "Maggie"
+action = "running"
+thing = "small archway"
+place = "Walmart"
+time = "noon"
+
+console.log(character + " was " + action + " down the street at " + time + " when suddenly they noticed a " + thing + ". " + "As " + character + " went through the " + thing +", " + character + " was transported to " + place + " where all the workers were getting ready for " + holdiay +".");
+
+
+
---
+++ scripts/script.js
@@ -0,0 +1,56 @@
+console.log("Script");
+
+//string ""
+let firstName = "Nick";
+let lastName = "Greenfield";
+
+//numbers
+let myAge = 29;
+let myShoe = 10.5;
+
+//boolean
+let isStudent = true; //false
+
+//display
+console.log(firstName);
+console.log(myAge)
+
+//concatenation
+console.log("My name is " + firstName + " and my age is " + myAge + " and I wear a size " + myShoe + " shoe");
+
+let num1 = 10;
+let num2 = 20;
+
+let addition = num1 + num2;
+let subs = num1 - num2;
+let mult = num1 * num2;
+let div = num1/num2;
+
+console.log("Addition is: " + addition);
+console.log("Subtraction is: " + subs);
+console.log("Multiplication is: " + mult);
+console.log("Division is: " + div)
+
+const pi= 3.1416
+let r = prompt("Enter the radius")
+let area = (r*r) * pi
+
+console.log("The area of a circle with a radius of " + r + " is: " + area +"cm");
+
+// define the var
+let aVariable;
+
+//define and assisting the value at the same time
+let otherVariable = 30;
+
+console.log(aVariable,otherVariable);
+
+aVariable=50;
+otherVariable= 100;
+
+console.log(aVariable,otherVariable)
+
+const weekDays = 7;
+console.log(weekDays)
+
+
